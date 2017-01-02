cf665b890109877bee5de5a9693da891a2c9d0e9
Szymon Urbaś <szymon.urbas@aol.com>
2017-01-02 21:06:47 +0100

    Accept user input in a little prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+extern crate getopts;
+use getopts::Options;
+use std::env;
+use std::io::{self, Write, BufRead};
+
+fn print_usage(program: &str, opts: Options)
+{
+  let brief = format!("Usage: {} [options]", program);
+
+  print!("{}", opts.usage(&brief));
 }
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let program = args[0].clone();
+
+  let mut opts = Options::new();
+  opts.optflag("h", "help", "print this help menu");
+
+  let matches = match opts.parse(&args[1..]) {
+    Ok(m) => { m }
+    Err(f) => { panic!(f.to_string()) }
+  };
+
+  if matches.opt_present("h") {
+    print_usage(&program, opts);
+    return;
+  }
+
+  let stdio = io::stdin();
+  let mut input = String::new();
+
+  loop {
+    print!("=> ");
+    io::stdout().flush().ok().expect("Could not flush stdout");
+
+    match stdio.lock().read_line(&mut input) {
+      Err(err) => println!("# error: {}", err),
+      Ok(_) => (),
+    }
+
+    println!("your input: {}", input);
+
+    input.clear();
+  }
+}
+
+/*
+ * vi: sw=2:ts=2 expandtab
+ */
+
