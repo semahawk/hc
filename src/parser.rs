fc6021b20494c240e55899a0fe8fc2edbe98b05f
Szymon Urbaś <szymon.urbas@aol.com>
2017-01-06 16:27:55 +0100

    Provide a macro for defining binary operations in the parser

--- src/parser.rs
+++ src/parser.rs
@@ -20,6 +20,40 @@ pub enum Expr {
   Number(u32),
 }
 
+macro_rules! generate_binop {
+  ($high_fname:ident before $func_name:ident, $op1:ident -> $expr1:ident, $op2:ident -> $expr2:ident) => (
+    fn $func_name(mut tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
+      let mut lhs = $high_fname(&mut tokens);
+
+      if lhs.is_none() {
+        return None;
+      }
+
+      while let Some(op) = tokens.peeking_take_while(|t| match t { &&Token::$op1 => true, &&Token::$op2 => true, _ => false }).next() {
+        let rhs = $high_fname(&mut tokens);
+
+        if rhs.is_none() {
+          return None;
+        }
+
+        lhs = match op {
+          &Token::$op1 => {
+            Some(Expr::$expr1(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
+          },
+          &Token::$op2 => {
+            Some(Expr::$expr2(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
+          },
+          _ => {
+            None
+          },
+        };
+      };
+
+      lhs
+    }
+  )
+}
+
 fn primary(tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
   if tokens.peek().is_none() {
     return None;
@@ -43,65 +77,8 @@ fn primary(tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
   }
 }
 
-fn mul(mut tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
-  let mut lhs = primary(&mut tokens);
-
-  if lhs.is_none() {
-    return None;
-  }
-
-  while let Some(op) = tokens.peeking_take_while(|t| match t { &&Token::Star => true, &&Token::Slash => true, _ => false }).next() {
-    let rhs = primary(&mut tokens);
-
-    if rhs.is_none() {
-      return None;
-    }
-
-    lhs = match op {
-      &Token::Star => {
-        Some(Expr::Mul(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
-      },
-      &Token::Slash => {
-        Some(Expr::Div(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
-      },
-      _ => {
-        None
-      },
-    };
-  };
-
-  lhs
-}
-
-fn add(mut tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
-  let mut lhs = mul(&mut tokens);
-
-  if lhs.is_none() {
-    return None;
-  }
-
-  while let Some(op) = tokens.peeking_take_while(|t| match t { &&Token::Plus => true, &&Token::Minus => true, _ => false }).next() {
-    let rhs = mul(&mut tokens);
-
-    if rhs.is_none() {
-      return None;
-    }
-
-    lhs = match op {
-      &Token::Plus => {
-        Some(Expr::Add(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
-      },
-      &Token::Minus => {
-        Some(Expr::Sub(Box::new(lhs.unwrap()), Box::new(rhs.unwrap())))
-      },
-      _ => {
-        None
-      },
-    };
-  };
-
-  lhs
-}
+generate_binop!(primary before mul, Star -> Mul, Slash -> Div);
+generate_binop!(mul before add, Plus -> Add, Minus -> Sub);
 
 fn assign(mut tokens: &mut Peekable<Iter<Token>>) -> Option<Expr> {
   let mut lhs = add(&mut tokens);
